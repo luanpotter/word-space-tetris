1f4ceab40cd513ecad50a9734879922242223940
Luan Nico <luanpotter27@gmail.com>
2018-12-15 04:29:11 -0200

    adding button

--- lib/components/letter.dart
+++ lib/components/letter.dart
@@ -10,6 +10,7 @@ import '../mixins/has_game_ref.dart';
 class Letter extends SpriteComponent with Resizable, HasGameRef {
   int column;
   String letter;
+  bool cold = false;
 
   Letter(this.column, this.letter) : super.fromSprite(32.0, 32.0, _sprite(letter)) {
     y = 0;
@@ -22,11 +23,15 @@ class Letter extends SpriteComponent with Resizable, HasGameRef {
 
   @override
   void update(double t) {
+    if (cold) {
+      return;
+    }
     y += 256 * t;
     int myColumnStack = gameRef.lastColumns[this.column] + 1;
     if (y > size.height - myColumnStack*height) {
       gameRef.lastColumns[this.column]++;
       y = size.height - myColumnStack*height;
+      cold = true;
     }
   }
 
